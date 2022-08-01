dd4f88b9449f70f083c2c0b10f41975830b065e9
huan5678 <huan5678@gmail.com>
2022-08-01 13:26:22 +0800

    Update 2022-08-01T05:26:16.944Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,2 +1,17 @@
 "use strict";
+
+var state = 'closed';
+$('.menu').on('click', function () {
+  $('.nav__list').toggleClass('max-h-0 h-0 pb-4');
+
+  if (state === 'open') {
+    $('.menu').children("span:contains('close')").text('menu');
+    return state = 'closed';
+  }
+
+  if (state === 'closed') {
+    $('.menu').children("span:contains('menu')").text('close');
+    return state = 'open';
+  }
+});
 //# sourceMappingURL=all.js.map
